f9f880a7917435b08ecb7f8bd5df4e74c83662cb
Jiri Bakker <jiri.bakker@infi.nl>
2016-09-16 13:03:23 +0200

    Implemented filtering on date range (dateFrom - dateUntil) when querying for Payments

--- FSharp.SpendingTracker.Data/Repositories/PaymentRepository.fs
+++ FSharp.SpendingTracker.Data/Repositories/PaymentRepository.fs
@@ -6,17 +6,22 @@ open FSharp.SpendingTracker.Data.Records
 
 module PaymentRepository =    
 
-    let getPayments (dateFrom:DateTimeOffset option) (dateUntil:DateTimeOffset option) connection =
+    let getPayments (dateFrom:DateTimeOffset option) (dateUntil:DateTimeOffset option) connection =        
+
         let timestampClauseIfSome timestampOption clauseSuffix =
             match timestampOption with
             | None            -> ""
             | Some(timestamp) -> " AND [Timestamp] " + clauseSuffix
 
         let dateFromClause  = timestampClauseIfSome dateFrom  ">= @dateFrom"
-        let dateUntilClause = timestampClauseIfSome dateUntil "<= @dateUntil"            
+        let dateUntilClause = timestampClauseIfSome dateUntil "<= @dateUntil"           
+        
+        let dateRangeParameters =
+            [("dateFrom", dateFrom); ("dateUntil", dateUntil)]
+            |> List.fold (fun acc param -> match param with | (_,None) -> acc | (k,Some(p)) -> (k,p :> obj) :: acc) []
 
         connection
-        |> dapperQuery<PaymentRecord> "SELECT * FROM [Payment] WHERE (1=1) "
+        |> dapperMapParametrizedQuery<PaymentRecord> ("SELECT * FROM [Payment] WHERE (1=1) " + dateFromClause + dateUntilClause) (Map dateRangeParameters)
         |> List.ofSeq
         |> List.map (fun paymentRecord -> paymentRecord.MapTo)
     
